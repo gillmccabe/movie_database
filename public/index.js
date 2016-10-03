9fb66a9f9dd5f5500a234688688b811756c49a8d
Gillian McCabe <gillianmccabe@outlook.com>
2016-10-03 07:52:43 +0100

    changes to display result

--- public/index.js
+++ public/index.js
@@ -5,18 +5,10 @@ var makeRequest = function(url, callback) {
   request.send(); 
 }
 
-var populateYearDropdown = function(){
-  var date = new Date().getFullYear();
-  var selector = document.getElementById('year-selector');
-  var i = 0; 
-  do {
-    var option = document.createElement("option");
-    option.textContent = date - i;
-    option.value = date - i;
-    selector.appendChild(option);
-    i++;
-  } while (i < 50)
-
+var urlRequestBuilder = function(title, year = null, type = null){
+  var requestUrl = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&type=' + type;
+  console.log(requestUrl);
+  return requestUrl; 
 }
 
 var searchMovie = function(){
@@ -25,12 +17,6 @@ var searchMovie = function(){
   var year = document.querySelector('#year-selector').value;
   var type = document.querySelector('#type-selector').value;
   makeRequest(urlRequestBuilder(title, year, type), requestMovieComplete);
-}
-
-var urlRequestBuilder = function(title, year = null, type = null){
-  var requestUrl = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&type=' + type;
-  console.log(requestUrl);
-  return requestUrl; 
 }
 
 var requestMovieComplete = function (){
@@ -46,24 +32,32 @@ var requestMovieComplete = function (){
   toggleLoadingSpinner('#submit-btn', '#loading-spinner');
 }
 
-var displayNoResult = function (){
-  if (!this.title) {
-    document.getElementById('r-info').innerText = 'Your search returned no results. Please try again.';
-    document.getElementById('r-poster').style.visibility = 'hidden';
-  }
-}
 
 var displayResult = function (response){
+  // if (!this.response) {
+  //   document.getElementById('r-info').innerText = 'Your search returned no results. Please try again.';
+  //   document.getElementById('r-poster').style.visibility = 'hidden';
+  // } 
+  // else {
   document.getElementById('r-info').innerText = 
   "Title: \n" + response.Title + " \n \n Year: \n" + response.Year + "\n \n Type: \n" + response.Type + "\n \n Synopsis: \n " + response.Plot;
   var poster = document.getElementById('r-poster');
-  if(response.Poster == "N/A"){
-    poster.style.visibility = "hidden";
-  }
-  else{
     poster.style.visibility = "visible";
     poster.src = response.Poster;
-  }
+  // }
+}
+
+var populateYearDropdown = function(){
+  var date = new Date().getFullYear();
+  var selector = document.getElementById('year-selector');
+  var i = 0; 
+  do {
+    var option = document.createElement("option");
+    option.textContent = date - i;
+    option.value = date - i;
+    selector.appendChild(option);
+    i++;
+  } while (i < 50)
 }
 
 var toggleLoadingSpinner = function (btn, spnr){
@@ -73,10 +67,10 @@ var toggleLoadingSpinner = function (btn, spnr){
   spinner.hidden = !spinner.hidden;
 }
 
-// var app = function(){
-//   var url = "http://hp-api.herokuapp.com/api/characters";
+var app = function(){
+//   var url = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&type=' + type;
   //makeRequest(url, requestComplete);  
-//   populateYearDropdown();
-// }
+  populateYearDropdown();
+}
 
-// window.onload = app;
+window.onload = app;
